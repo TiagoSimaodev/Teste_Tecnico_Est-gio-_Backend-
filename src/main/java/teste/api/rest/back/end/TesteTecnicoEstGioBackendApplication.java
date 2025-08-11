6607b246ce3da2d2f43afd1867c7f2b1cec2916d
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-11 20:03:22 -0300

    commit inicial, teste tecnico back end java

---
+++ src/main/java/teste/api/rest/back/end/TesteTecnicoEstGioBackendApplication.java
@@ -0,0 +1,13 @@
+package teste.api.rest.back.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoEstGioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoEstGioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste/api/rest/back/end/TesteTecnicoEstGioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.api.rest.back.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoEstGioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
