dcbb84c44f6f9869c5a4d30b8aa1d14179d7b676
tiago simao <tiagosimaorodri123@gmail.com>
2025-08-12 00:37:57 -0300

    feita umas correções de regra de negocio

--- src/main/java/teste/api/rest/back/end/model/Tarefa.java
+++ src/main/java/teste/api/rest/back/end/model/Tarefa.java
@@ -3,7 +3,6 @@ package teste.api.rest.back.end.model;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -12,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -39,8 +39,8 @@ public class Tarefa {
 	
 	public enum StatusTarefas {
 		PENDENTE,
-		CONLUIDA,
-		CANCELADA
+		CONCLUIDO,
+		CANCELADO
 	}
 	
 	public enum PrioridadeTarefas {
